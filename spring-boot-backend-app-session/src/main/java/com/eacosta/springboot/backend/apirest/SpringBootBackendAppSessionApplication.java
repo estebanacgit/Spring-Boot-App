46dc3aa7cde5d4eac8442af4229c5371f7fceddd
Esteban <estebanacosta994@gmail.com>
2022-11-05 22:58:41 -0300

    Commit

---
+++ spring-boot-backend-app-session/src/main/java/com/eacosta/springboot/backend/apirest/SpringBootBackendAppSessionApplication.java
@@ -0,0 +1,13 @@
+package com.eacosta.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendAppSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendAppSessionApplication.class, args);
+	}
+
+}
---
+++ spring-boot-backend-app-session/src/test/java/com/eacosta/springboot/backend/apirest/SpringBootBackendAppSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eacosta.springboot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendAppSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
